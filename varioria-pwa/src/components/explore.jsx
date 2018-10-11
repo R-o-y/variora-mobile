e6fea65cad55ef005c03a516692a66abaadd5771
Lin Han Yong <lhlhyong@gmail.com>
2018-10-11 10:47:45 +0800

    implement grid view for explore docs

--- varioria-pwa/src/components/explore.jsx
+++ varioria-pwa/src/components/explore.jsx
@@ -1,22 +1,99 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import * as actions from '../actions';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Navbar from './nav_bar';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+import { Tabs, WhiteSpace, Grid } from 'antd-mobile';
+import { StickyContainer, Sticky } from 'react-sticky';
 
 class Explore extends Component {
 
   componentDidMount() {
     this.props.getExploreDocuments();
     this.props.getExploreReadlists();
-    console.log("HERE");
-    console.log(this.props.explore);
+  }
+
+  renderReactSticky(props) {
+    return (
+      <Sticky>
+        {({ style }) =>
+          <div style={{ ...style, zIndex: 1 }}>
+            <Tabs.DefaultTabBar {...props} />
+          </div>
+        }
+      </Sticky>
+    )
+  }
+
+  renderExploreSubgrid(documents, title) {
+    const data = documents.map((element) => ({
+      icon: element.image,
+      text: element.title,
+      url: element.open_url,
+    }));
+    return (
+      <div>
+        <div style={{color: '#888', fontSize: '14px', padding: '15px 0 9px 15px'}}>{title}</div>
+        <Grid data={data} isCarousel onClick={(element) => {this.props.history.push(`${element.url}`)}} />
+      </div>
+    )
+
+  }
+
+  renderExploreDocuments(list) {
+    return (
+      <div>
+        {this.renderExploreSubgrid(list.mostViewsDocuments, "Most Views")}
+        {this.renderExploreSubgrid(list.mostStarsDocuments, "Most Stars")}
+        {this.renderExploreSubgrid(list.mostAnnotationsDocuments, "Most Annotations")}
+      </div>
+    )
+  }
+
+  renderExploreReadlists(list) {
+
+  }
+
+  renderStickyTab() {
+    return (
+      <div>
+        <WhiteSpace />
+        <StickyContainer>
+          <Tabs
+            tabs={[{ title: "Documents"}, { title: "Readlists"}]}
+            initalPage={'t2'}
+            renderTabBar={this.renderReactSticky}
+          >
+            <div style={{ justifyContent: 'center', height: '100%', backgroundColor: '#fff' }}>
+              {this.renderExploreDocuments(this.props.explore.documents)}
+            </div>
+            <div style={{ justifyContent: 'center', height: '100%', backgroundColor: '#fff' }}>
+              {this.renderExploreReadlists(this.props.explore.readlists)}
+            </div>
+          </Tabs>
+        </StickyContainer>
+        <WhiteSpace />
+      </div>
+    )
   }
 
   render() {
+    if (_.isEmpty(this.props.explore) || _.isEmpty(this.props.explore.documents) || _.isEmpty(this.props.explore.readlists)) {
+      return (
+        <div>
+          <Navbar title="Explore" />
+          <CircularProgress style={{color:"#1BA39C",  marginTop: "40vh"}} size={100} thickness={5} />
+        </div>
+      );
+    }
+
     return (
       <div>
         <Navbar title="Explore" />
+        {this.renderStickyTab()}
       </div>
     );
   }
--- varioria-pwa/src/reducers/reducer_explore.js
+++ varioria-pwa/src/reducers/reducer_explore.js
@@ -10,18 +10,22 @@ export default function (state = [], action) {
       const mostViewsDocuments = action.payload.data.filter(item => item.description == 'most_views');
       const mostStarsDocuments = action.payload.data.filter(item => item.description == 'most_collectors');
       const mostAnnotationsDocuments = action.payload.data.filter(item => item.description == 'most_annotations');
-      return _.extend({}, state,
+      return _.extend({}, state, {
+        documents: 
         { mostViewsDocuments: mostViewsDocuments,
           mostStarsDocuments: mostStarsDocuments,
           mostAnnotationsDocuments: mostAnnotationsDocuments,
-        });
+        }
+      });
     case READLIST_GET_EXPLORE:
       const mostCollectorsReadlists = action.payload.data.most_collectors_readlists;
       const newestReadlists = action.payload.data.newest_readlists;
-      return _.extend({}, state,
+      return _.extend({}, state, {
+        readlists:
         { mostCollectorsReadlists: mostCollectorsReadlists,
           newestReadlists: newestReadlists,
-        });
+        }
+      });
     default:
       return state;
   }
