872074e9d6c984acdd706512d798fa38acbe03c7
Lin Han Yong <lhlhyong@gmail.com>
2018-11-02 16:43:29 +0800

    if in group, disable add to readlist

--- varioria-pwa/src/components/uploads/uploads.jsx
+++ varioria-pwa/src/components/uploads/uploads.jsx
@@ -297,13 +297,18 @@ class Uploads extends Component {
           }
           className="popup-list"
         >
-          <List.Item
-            onClick={() => {
-              this.props.history.push('/add-to-readlists')
-            }}>
-            <AddIcon style={{height: 18, color:'#1BA39C',marginRight: 20}}/>
-            Add To Readlist
-          </List.Item>
+          { 
+            // ===== temporarily disable add to list if in group =====
+            this.props.match.params.groupUuid ? null :
+            // =======================================================
+            <List.Item
+              onClick={() => {
+                this.props.history.push('/add-to-readlists')
+              }}>
+              <AddIcon style={{height: 18, color:'#1BA39C',marginRight: 20}}/>
+              Add To Readlist
+            </List.Item>
+          }
           {
             this.props.match.params.groupUuid ? null :
             <List.Item
