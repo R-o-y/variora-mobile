9f228ec13c7983e8dfe802f976f981f2486d4025
cysjonathan <cysjonathan@gmail.com>
2018-11-08 09:00:07 +0800

    Prevent overscroll of annotation drawer, only works for Chrome Firefox and Edge. Does not work for Safari. See https://caniuse.com/#feat=css-overscroll-behavior

--- varioria-pwa/src/components/document_viewer/document_viewer.jsx
+++ varioria-pwa/src/components/document_viewer/document_viewer.jsx
@@ -625,7 +625,6 @@ class DocumentViewer extends React.Component {
           onClose={() => this.deselectAnnotation()}
           // ModalProps={{BackdropProps: {invisible: true}}}
           variant='persistent'
-          className='test'
         >
           <div>
             <Grid container justify='space-between' className='annotation-thread-navbar'>
